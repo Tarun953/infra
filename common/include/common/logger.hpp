aa0c18becd3e61db01cbfb6182b6f290cf7d1713
Tarun953 <tarunmisra953@gmail.com>
2026-04-14 08:16:28 +0000

    minor changes in logger

--- common/include/common/logger.hpp
+++ common/include/common/logger.hpp
@@ -18,7 +18,7 @@ namespace infra
             containers::SPSCQueue<std::string, 65536> log_queue_{};
             std::string log_directory_{};
             std::thread log_thread_;
-            std::fstream log_file_;
+            std::ofstream log_file_;
             std::atomic<bool> running_{false};
 
             Logger() = default;
@@ -26,11 +26,13 @@ namespace infra
             void run()
             {
                 std::string log_entry;
+                log_entry.reserve(1024); // Reserve space to avoid frequent reallocations
                 while (running_.load(std::memory_order_acquire) || !log_queue_.empty())
                 {
                     if (log_queue_.pop(log_entry))
                     {
-                        log_file_ << log_entry << std::endl;
+                        // remove std::endl to avoid flushing the stream on every log entry, which can significantly improve performance
+                        log_file_ << log_entry << "\n";
                     }
                 }
             }
@@ -49,19 +51,21 @@ namespace infra
                     return; // Logger is already initialized
                 }
                 log_directory_ = log_directory;
-                running_.store(true, std::memory_order_release);
                 log_file_.open(log_directory_ + "/" + log_file_name, overwrite ? std::ios::out : std::ios::out | std::ios::app);
                 if (!log_file_.is_open())
                 {
                     throw std::runtime_error("Failed to open log file");
-                    exit(0);
                 }
+                running_.store(true, std::memory_order_release);
                 log_thread_ = std::thread(&Logger::run, this);
             }
 
-            void log(const std::string &message)
+            template <typename... Args>
+            void log(Args &&...args)
             {
-                while (!log_queue_.push(std::move(message)))
+                std::ostringstream oss;
+                (oss << ... << std::forward<Args>(args)); // Fold expression to concatenate all arguments into a single string
+                while (!log_queue_.push(std::move(oss.str())))
                     ;
             }
 
